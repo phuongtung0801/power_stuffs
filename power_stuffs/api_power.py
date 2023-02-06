6a0644c5f573fa90f600d6638b435df40cdb45f4
phuongtung0801 <phuongtung0801@gmail.com>
2023-02-06 15:47:43 +0700

    update api inverter

--- power_stuffs/api_power.py
+++ power_stuffs/api_power.py
@@ -2,7 +2,7 @@
 import datetime
 from dateutil.relativedelta import relativedelta
 import frappe
-
+#########SITE API###############
 @frappe.whitelist()
 def site_power_daily(param):
     dateFrom = param["date"]
@@ -129,6 +129,134 @@ def site_power_quarter(param):
 
 
 
+
+
+
+
+#########INVERTER API###############
+@frappe.whitelist()
+def inverter_power_daily(param):
+    dateFrom = param["date"]
+    inverter_name = param["inverter_name"]
+    
+    # strptime(input_string, input_format)
+    date_time_obj = datetime.datetime.strptime(dateFrom, '%Y-%m-%d')
+    year = date_time_obj.year
+    month = date_time_obj.month
+    day = date_time_obj.day
+    #######
+    powerMonthlyArr  = []
+    dateStringStart = datetime.datetime(year, month, day, 00, 00, 00)
+    dateStringEnd = datetime.datetime(year, month, day, 23, 59, 59)
+    queryString = f"""select inverter_name, power_per_hour, `from`, `to` from `tabInverter Power Per Hour`  where inverter_name = "{inverter_name}" and `from` > "{dateStringStart}" and `to` < "{dateStringEnd}" order by `from` asc"""
+    docArr = frappe.db.sql(queryString)
+    for element in docArr:
+        jsonObjDay = {
+            "inverter_name": element[0],
+            "power": element[1],
+            "from": element[2],
+            "to": element[3]
+        }
+        powerMonthlyArr.append(jsonObjDay)
+    response = {
+        "length": len(powerMonthlyArr),
+        "body": powerMonthlyArr
+    }
+    return response 
+
+
+@frappe.whitelist()
+def inverter_power_weekly(param):
+    dateFrom = param["date"]
+    inverter_name = param["inverter_name"]
+    
+    # strptime(input_string, input_format)
+    date_time_obj = datetime.datetime.strptime(dateFrom, '%Y-%m-%d')
+    year = date_time_obj.year
+    month = date_time_obj.month
+    day = date_time_obj.day
+    #######
+    powerMonthlyArr  = []
+    dateStringStart = datetime.datetime(year, month, day, 00, 00, 00)
+    dateStringEnd = datetime.datetime(year, month, day + 7, 23, 59, 59)
+    queryString = f"""select inverter_name, power_per_hour, `from`, `to` from `tabInverter Power Per Hour`  where inverter_name = "{inverter_name}" and `from` > "{dateStringStart}" and `to` < "{dateStringEnd}" order by `from` asc"""
+    docArr = frappe.db.sql(queryString)
+    for element in docArr:
+        jsonObjDay = {
+            "inverter_name": element[0],
+            "power": element[1],
+            "from": element[2],
+            "to": element[3]
+        }
+        powerMonthlyArr.append(jsonObjDay)
+    response = {
+        "length": len(powerMonthlyArr),
+        "body": powerMonthlyArr
+    }
+    return response 
+
+
+
+@frappe.whitelist()
+def inverter_power_monthly(param):
+    dateFrom = param["date"]
+    inverter_name = param["inverter_name"]
+    
+    # strptime(input_string, input_format)
+    date_time_obj = datetime.datetime.strptime(dateFrom, '%Y-%m-%d')
+    year = date_time_obj.year
+    month = date_time_obj.month
+    day = date_time_obj.day
+    #######
+    powerMonthlyArr  = []
+    dateStringStart = datetime.datetime(year, month, day, 00, 00, 00)
+    dateStringEnd = dateStringStart + relativedelta(months=1, days= -1, hours= 23, minutes=59)
+    queryString = f"""select inverter_name, power_per_hour, `from`, `to` from `tabInverter Power Per Hour`  where inverter_name = "{inverter_name}" and `from` > "{dateStringStart}" and `to` < "{dateStringEnd}" order by `from` asc"""
+    docArr = frappe.db.sql(queryString)
+    for element in docArr:
+        jsonObjDay = {
+            "inverter_name": element[0],
+            "power": element[1],
+            "from": element[2],
+            "to": element[3]
+        }
+        powerMonthlyArr.append(jsonObjDay)
+    response = {
+        "length": len(powerMonthlyArr),
+        "body": powerMonthlyArr
+    }
+    return response 
+
+
+@frappe.whitelist()
+def inverter_power_quarter(param):
+    dateFrom = param["date"]
+    inverter_name = param["inverter_name"]
+    
+    # strptime(input_string, input_format)
+    date_time_obj = datetime.datetime.strptime(dateFrom, '%Y-%m-%d')
+    year = date_time_obj.year
+    month = date_time_obj.month
+    day = date_time_obj.day
+    #######
+    powerMonthlyArr  = []
+    dateStringStart = datetime.datetime(year, month, day, 00, 00, 00)
+    dateStringEnd = dateStringStart + relativedelta(months=4, days= -1, hours= 23, minutes=59)
+    queryString = f"""select inverter_name, power_per_hour, `from`, `to` from `tabInverter Power Per Hour`  where inverter_name = "{inverter_name}" and `from` > "{dateStringStart}" and `to` < "{dateStringEnd}" order by `from` asc"""
+    docArr = frappe.db.sql(queryString)
+    for element in docArr:
+        jsonObjDay = {
+            "inverter_name": element[0],
+            "power": element[1],
+            "from": element[2],
+            "to": element[3]
+        }
+        powerMonthlyArr.append(jsonObjDay)
+    response = {
+        "length": len(powerMonthlyArr),
+        "body": powerMonthlyArr
+    }
+    return response 
 
 
 
